1b5f690f1ddd866471bb29795aee156f0ca5b8f3
Loai Kanou <loaikanou@gmail.com>
2023-08-12 22:04:22 +0300

    init

--- search.py
+++ search.py
@@ -3,13 +3,14 @@ from csv import DictReader
 
 
 class main:
-    args = sys.argv[1:]
     counter = 0
+    search_input = ""
 
-    if not args[0]:
-        search_input = input("Type Language Name To Search? ").lower().strip()
-    else:
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
         search_input = args[0].lower().strip()
+    else:
+        search_input = input("Type Language Name To Search? ").lower().strip()
 
     with open('favorites.csv', 'r') as file:
         reader = DictReader(file)
